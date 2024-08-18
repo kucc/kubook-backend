809e42392e80272eee6e4262d01d9b22e9f68de5
임유나 <youna0104@naver.com>
2024-08-18 19:06:41 +0900

    feat: import 수정 - domain/services/user_service.py 대신에 admin_service.py 사용

--- src/routes/user_book_request_route.py
+++ src/routes/user_book_request_route.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import Session
 from routes.request.update_book_request_request import UpdateBookRequest as req
 from routes.response.book_request_response import BookRequest as res
 from dependencies import get_current_active_user, get_db
-from domain.services.user_service import *
+from domain.services.admin_service import update_item
 from repositories.requested_book import RequestedBook
 
 router = APIRouter(
@@ -23,4 +23,4 @@ router = APIRouter(
     status_code=status.HTTP_200_OK
 )
 async def update_user_book_request(user_id: int, request_id: int, request_data: req, db: Session = Depends(get_db), get_current_active_user=Depends(get_current_active_user)):
-    return update_item(RequestedBook, request_id, request_data, db)  # user_service.py 미개발
+    return update_item(RequestedBook, request_id, request_data, db)
