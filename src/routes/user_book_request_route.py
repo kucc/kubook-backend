fc586dcd03e9186a64990f45264f690187b36149
임유나 <youna0104@naver.com>
2024-08-28 22:58:46 +0900

    - domain 스키마 attribute 이름 수정 - 안쓰는 스키마 제거

--- src/routes/user_book_request_route.py
+++ src/routes/user_book_request_route.py
@@ -3,10 +3,10 @@ from typing import List
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
-from routes.request.update_book_request_request import UpdateBookRequest as route_update_bookreq_req
+from routes.request.update_book_request_request import UpdateBookRequest as route_bookreq_req
 from routes.response.book_request_response import BookRequestResponse as route_bookreq_res
 
-from domain.schemas.book_request_schemas import BookRequestUpdateRequest as domain_update_bookreq_req
+from domain.schemas.book_request_schemas import BookRequestRequest as domain_bookreq_req
 from domain.services.book_request_service import update as update_bookreq
 
 from dependencies import get_current_active_user, get_db
@@ -28,13 +28,13 @@ router = APIRouter(
 async def update_user_book_request(
     user_id: int,
     request_id: int,
-    request_data: route_update_bookreq_req,
+    request_data: route_bookreq_req,
     db: Session = Depends(get_db),
     status_code=status.HTTP_200_OK
 ):
-    domain_req = domain_update_bookreq_req(
+    domain_req = domain_bookreq_req(
         user_id=user_id,
-        id=request_id,
+        request_id=request_id,
         book_title=request_data.book_title,
         publication_year=request_data.publication_year,
         request_link=request_data.request_link,
@@ -42,8 +42,5 @@ async def update_user_book_request(
     )
 
     domain_res = await update_bookreq(domain_req, db)
-
-    if not domain_res:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Request not found.")
-
-    return {"status_code": status_code, "data": domain_res}
+    route_res = route_bookreq_res(data=domain_res)
+    return route_res
