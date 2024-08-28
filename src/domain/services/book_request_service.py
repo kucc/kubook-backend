1918f318482b8b3a97a55f4ce83743fd60f5d97c
임유나 <youna0104@naver.com>
2024-08-28 22:58:46 +0900

    - domain 스키마 attribute 이름 수정 - 안쓰는 스키마 제거

--- src/domain/services/book_request_service.py
+++ src/domain/services/book_request_service.py
@@ -1,33 +1,30 @@
 from typing import Any, Dict
 
 from fastapi import HTTPException, status
-from sqlalchemy import delete, select, update
-from sqlalchemy.exc import IntegrityError, NoResultFound
 from sqlalchemy.orm import Session
 
-from domain.schemas.book_request_schemas import BookRequestUpdateRequest as update_bookreq_req
-from domain.schemas.book_request_schemas import BookRequestUpdateResponse as update_bookreq_res
+from domain.schemas.book_request_schemas import BookRequestRequest as bookreq_req
+from domain.schemas.book_request_schemas import BookRequestResponse as bookreq_res
 from repositories.requested_book import RequestedBook
 from repositories.user_repository import User
-from domain.services.user_service import update_item, get_item
+from src.utils.user_service import update_item, get_item
 
 
-async def update(request_data: update_bookreq_req, db: Session):
-    requested_book = db.query(RequestedBook).filter(RequestedBook.id == request_data.id).first()
+async def update(request_data: bookreq_req, db: Session):
+    requested_book = get_item(RequestedBook, request_data.request_id, db)
 
     if not requested_book:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Requested book not found")
 
     # 도서 구매 요청 table 수정
-    update_item(RequestedBook, request_data.id, request_data, db)
-    # 요청한 user table 수정
-    update_item(User, request_data.user_id, request_data, db)
+    update_item(RequestedBook, request_data.request_id, request_data, db)
 
-    updated_book = get_item(RequestedBook, request_data.id, db)
+    updated_book = get_item(RequestedBook, request_data.request_id, db)
 
     # domain response schema 생성
-    response = update_bookreq_res(
+    response = bookreq_res(
         user_id=updated_book.user_id,
+        request_id=updated_book.request_id,
         book_title=updated_book.book_title,
         publication_year=updated_book.publication_year,
         request_link=updated_book.request_link,
--- src/routes/user_book_request_route.py
+++ src/routes/user_book_request_route.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
-from routes.request.update_book_request_request import UpdateBookRequest as route_update_bookreq_req
+from routes.request.update_book_request_request import UpdateBookRequest as route_bookreq_req
 from routes.response.book_request_response import BookRequestResponse as route_bookreq_res
-from domain.schemas.book_request_schemas import BookRequestUpdateRequest as domain_update_bookreq_req
-from domain.services.book_request_service import update
+from domain.schemas.book_request_schemas import BookRequestRequest as domain_bookreq_req
+from domain.services.book_request_service import update as update_bookreq
 from dependencies import get_current_active_user, get_db
 
 router = APIRouter(
@@ -22,17 +22,19 @@ router = APIRouter(
 async def update_user_book_request(
     user_id: int,
     request_id: int,
-    request_data: route_update_bookreq_req,
+    request_data: route_bookreq_req,
     db: Session = Depends(get_db),
     get_current_active_user=Depends(get_current_active_user)
 ):
-    domain_req = domain_update_bookreq_req(
+    domain_req = domain_bookreq_req(
         user_id=user_id,
-        id=request_id,
+        request_id=request_id,
         book_title=request_data.book_title,
         publication_year=request_data.publication_year,
         request_link=request_data.request_link,
         reason=request_data.reason,
     )
 
-    return await update(domain_req, db)
+    domain_res = await update_bookreq(domain_req, db)
+    route_res = route_bookreq_res(data=domain_res)
+    return route_res
