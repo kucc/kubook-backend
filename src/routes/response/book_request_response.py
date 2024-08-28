34510e0af808f28a67fff1d8bb836940cf5c012f
임유나 <youna0104@naver.com>
2024-08-28 14:58:39 +0900

    modified:   src/routes/response/book_request_response.py modified:   src/routes/user_book_request_route.py
    - route api에서 domain request schema를 만들어서 domain service 호출

--- src/routes/response/book_request_response.py
+++ src/routes/response/book_request_response.py
@@ -7,7 +7,7 @@ from pydantic import Field
 from pydantic import BaseModel, Field
 
 
-class BookRequest(BaseModel):
+class BookRequestResponse(BaseModel):
     book_title: str = Field(..., title="book_title", description="책 제목", example="FastAPI Tutorial")
     publication_year: int = Field(..., title="publication_year", description="출판년도", example=2022, ge=0)
     request_link: str = Field(..., title="request_link", description="요청 링크", example="https://example.com/request")
--- src/routes/user_book_request_route.py
+++ src/routes/user_book_request_route.py
@@ -3,9 +3,11 @@ from typing import List
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
-from routes.request.update_book_request_request import UpdateBookRequest as req
-from routes.response.book_request_response import BookRequest as res
-import domain.services.book_request_service as book_request_service
+from routes.request.update_book_request_request import UpdateBookRequest as route_update_bookreq_req
+from routes.response.book_request_response import BookRequestResponse as route_bookreq_res
+
+from domain.schemas.book_request_schemas import BookRequestUpdateRequest as domain_update_bookreq_req
+from domain.services.book_request_service import update
 
 from dependencies import get_current_active_user, get_db
 # from repositories.requested_book import RequestedBook
@@ -20,8 +22,23 @@ router = APIRouter(
 @router.put(
     "/{user_id}/book-requests/{requests_id}",
     summary="도서 구매 요청 수정",
-    response_model=res,
+    response_model=route_bookreq_res,
     status_code=status.HTTP_200_OK
 )
-async def update_user_book_request(user_id: int, request_id: int, request_data: req, db: Session = Depends(get_db), get_current_active_user=Depends(get_current_active_user)):
-    return await book_request_service.update(user_id, request_id, request_data, db)
+async def update_user_book_request(
+    user_id: int,
+    request_id: int,
+    request_data: route_update_bookreq_req,
+    db: Session = Depends(get_db),
+    get_current_active_user=Depends(get_current_active_user)
+):
+    domain_req = domain_update_bookreq_req(
+        user_id=user_id,
+        id=request_id,
+        book_title=request_data.book_title,
+        publication_year=request_data.publication_year,
+        request_link=request_data.request_link,
+        reason=request_data.reason,
+    )
+
+    return await update(domain_req, db)
