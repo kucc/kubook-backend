04a12a5bdb1027640cf186f05423f1cbc12f0a31
임유나 <youna0104@naver.com>
2024-08-28 15:19:43 +0900

    modified:   src/domain/services/book_request_service.py - import as 수정

--- src/domain/services/book_request_service.py
+++ src/domain/services/book_request_service.py
@@ -5,28 +5,28 @@ from sqlalchemy import delete, select, update
 from sqlalchemy.exc import IntegrityError, NoResultFound
 from sqlalchemy.orm import Session
 
-from domain.schemas.book_request_schemas import BookRequestUpdateRequest
-from domain.schemas.book_request_schemas import BookRequestUpdateResponse
+from domain.schemas.book_request_schemas import BookRequestUpdateRequest as update_bookreq_req
+from domain.schemas.book_request_schemas import BookRequestUpdateResponse as update_bookreq_res
 from repositories.requested_book import RequestedBook
 from repositories.user_repository import User
 from domain.services.user_service import update_item, get_item
 
 
-async def update(user_id, request_id, request_data, db):
-    requested_book = db.query(RequestedBook).filter(RequestedBook.id == request_id).first()
+async def update(request_data: update_bookreq_req, db: Session):
+    requested_book = db.query(RequestedBook).filter(RequestedBook.id == request_data.id).first()
 
     if not requested_book:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Requested book not found")
 
     # 도서 구매 요청 table 수정
-    update_item(RequestedBook, request_id, request_data, db)
+    update_item(RequestedBook, request_data.id, request_data, db)
     # 요청한 user table 수정
-    update_item(User, user_id, request_data, db)
+    update_item(User, request_data.user_id, request_data, db)
 
-    updated_book = get_item(RequestedBook, request_id, db)
+    updated_book = get_item(RequestedBook, request_data.id, db)
 
     # domain response schema 생성
-    response = BookRequestUpdateResponse(
+    response = update_bookreq_res(
         user_id=updated_book.user_id,
         book_title=updated_book.book_title,
         publication_year=updated_book.publication_year,
