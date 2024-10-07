a5c2236c0be83dd70db768d4cd25e708f5b3ac0f
Tiger-Muke <kimkw0911@korea.ac.kr>
2024-10-07 19:45:22 +0900

    style : 공백 제거 및 양식에 맞지 않는 조건 삭제

--- src/domain/schemas/user_schemas.py
+++ src/domain/schemas/user_schemas.py
@@ -2,28 +2,28 @@ from pydantic import BaseModel, Field
 from typing import Optional
 
 class DomainReqGetUser(BaseModel):
-    user_id: int = Field(..., title="user_id", description="유저 고유 ID", example=1111, gt=0)
+    user_id: int = Field(title="user_id", description="유저 고유 ID", example=1111, gt=0)
 
 class DomainResGetUser(BaseModel):
-    user_id: int = Field(..., title="user_id", description="유저 고유 ID", example=1111, gt=0)
-    auth_id: str = Field(..., title="auth_id", description="로그인 ID", example="gildong1")
-    email: str = Field(..., title="email", description="이메일 주소", example="KUCC@korea.ac.kr")
-    user_name: str = Field(..., title="user_name", description="사용자 이름", example="홍길동")
-    is_active: bool = Field(..., title="is_active", description="활동 상태", example=1)
-    github: Optional[str] = Field(None, title="github", description="깃허브 주소", example="https://github.com/kucc")
-    instagram: Optional[str] = Field(None, title="instagram", description="인스타그램 주소", example="https://www.instagram.com/")
+    user_id: int = Field(title="user_id", description="유저 고유 ID", example=1111, gt=0)
+    auth_id: str = Field(title="auth_id", description="로그인 ID", example="gildong1")
+    email: str = Field(title="email", description="이메일 주소", example="KUCC@korea.ac.kr")
+    user_name: str = Field(title="user_name", description="사용자 이름", example="홍길동")
+    is_active: bool = Field(title="is_active", description="활동 상태", example=1)
+    github: str | None = Field(None, title="github", description="깃허브 주소", example="https://github.com/kucc")
+    instagram: str | None = Field(None, title="instagram", description="인스타그램 주소", example="https://www.instagram.com/")
 
 class DomainReqPutUser(BaseModel):
-    user_id: int = Field(..., title="user_id", description="유저 고유 ID", example=1111, gt=0)
-    user_name: str = Field(..., title="user_name", description="사용자 이름", example="홍길동", gt = 0)
-    github: Optional[str] = Field(None, title="github", description="깃허브 주소", example="https://github.com/kucc")
-    instagram: Optional[str] = Field(None, title="instagram", description="인스타그램 주소", example="https://www.instagram.com/")
+    user_id: int = Field(title="user_id", description="유저 고유 ID", example=1111, gt=0)
+    user_name: str = Field(title="user_name", description="사용자 이름", example="홍길동")
+    github: str | None = Field(None, title="github", description="깃허브 주소", example="https://github.com/kucc")
+    instagram: str | None = Field(None, title="instagram", description="인스타그램 주소", example="https://www.instagram.com/")
     
 class DomainReqPutUser(BaseModel):
-    user_id: int = Field(..., title="user_id", description="유저 고유 ID", example=1111, gt=0)
-    auth_id: str = Field(..., title="auth_id", description="로그인 ID", example="gildong1", gt = 0)
-    email: str = Field(..., title="email", description="이메일 주소", example="KUCC@korea.ac.kr", gt = 0)
-    user_name: str = Field(..., title="user_name", description="사용자 이름", example="홍길동", gt = 0)
-    is_active: bool = Field(..., title="is_active", description="활동 상태", example=1)
-    github: Optional[str] = Field(None, title="github", description="깃허브 주소", example="https://github.com/kucc")
-    instagram: Optional[str] = Field(None, title="instagram", description="인스타그램 주소", example="https://www.instagram.com/")
+    user_id: int = Field(title="user_id", description="유저 고유 ID", example=1111, gt=0)
+    auth_id: str = Field(title="auth_id", description="로그인 ID", example="gildong1")
+    email: str = Field(title="email", description="이메일 주소", example="KUCC@korea.ac.kr")
+    user_name: str = Field(title="user_name", description="사용자 이름", example="홍길동")
+    is_active: bool = Field(title="is_active", description="활동 상태", example=1)
+    github: str | None = Field(None, title="github", description="깃허브 주소", example="https://github.com/kucc")
+    instagram: str | None = Field(None, title="instagram", description="인스타그램 주소", example="https://www.instagram.com/")
--- src/routes/request/user_request.py
+++ src/routes/request/user_request.py
@@ -3,5 +3,5 @@ from typing import Optional
 
 class RouteReqPutUser(BaseModel):
     user_name: str = Field(title="user_name", description="사용자 이름", example="홍길동")
-    github: Optional[str] = Field(None, title="github", description="깃허브 주소", example="https://github.com/kucc")
-    instagram: Optional[str] = Field(None, title="instagram", description="인스타그램 주소", example="https://www.instagram.com/")
+    github: str | None = Field(None, title="github", description="깃허브 주소", example="https://github.com/kucc")
+    instagram: str | None = Field(None, title="instagram", description="인스타그램 주소", example="https://www.instagram.com/")
