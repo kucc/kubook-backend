bd3b19c69c26512231961637357733de276e4d82
Tiger-Muke <kimkw0911@korea.ac.kr>
2024-10-09 14:18:22 +0900

    .

--- src/domain/schemas/user_schemas.py
+++ src/domain/schemas/user_schemas.py
@@ -19,7 +19,7 @@ class DomainReqPutUser(BaseModel):
     github: str | None = Field(None, title="github", description="깃허브 주소", example="https://github.com/kucc")
     instagram: str | None = Field(None, title="instagram", description="인스타그램 주소", example="https://www.instagram.com/")
     
-class DomainReqPutUser(BaseModel):
+class DomainResPutUser(BaseModel):
     user_id: int = Field(title="user_id", description="유저 고유 ID", example=1111, gt=0)
     auth_id: str = Field(title="auth_id", description="로그인 ID", example="gildong1")
     email: str = Field(title="email", description="이메일 주소", example="KUCC@korea.ac.kr")
