5501629c49bb28a9bc64f2cef6a6f5482581b6a4
임유나 <youna0104@naver.com>
2024-11-06 23:12:19 +0900

    fix: optional

--- src/domain/schemas/book_schemas.py
+++ src/domain/schemas/book_schemas.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Optional
 
 from pydantic import BaseModel, Field
 
@@ -39,12 +40,12 @@ class DomainReqAdminPostBook(BaseModel):
     book_title: str = Field(title="book_title", description="책 제목", example="FastAPI Tutorial")
     code: str = Field(title="code", description="책 코드", examples="A3")
     category_name: str = Field(title="category_name", description="카테고리명", examples="웹")
-    subtitle: str | None = Field(title="subtitle", description="책 부제")
-    autor : str = Field(title="author", description="저자")
+    subtitle: Optional[str] = Field(title="subtitle", description="책 부제", default=None)
+    author : str = Field(title="author", description="저자")
     publisher: str = Field(title="publisher", description="출판사")
     publication_year: int = Field(title="publication_year", description="출판연도", gt=0)
-    image_url: str | None = Field(title="image_url", description="책 표지")
-    version: str | None = Field(title="version", description="판본")
+    image_url: Optional[str] = Field(title="image_url", description="책 표지", default=None)
+    version: Optional[str] = Field(title="version", description="판본", default=None)
     major: bool = Field(title="major", description="전공 책 여부")
     language: str = Field(title="language", description="언어")
-    donor_name: str | None = Field(title="donor_name", description="기부자명")
+    donor_name: Optional[str] = Field(title="donor_name", description="기부자명", default=None)
