9da288adb2b2e225c7d2d5eaf61938dc5f14ac3e
임유나 <youna0104@naver.com>
2024-11-06 20:55:20 +0900

    feat: domain req admin post book 스키마 추가

--- src/domain/schemas/book_schemas.py
+++ src/domain/schemas/book_schemas.py
@@ -34,3 +34,17 @@ class DomainResGetBookList(BaseModel):
     book_status: bool = Field(title="book_stauts", description="책 상태", example=True)
     created_at: datetime = Field(title="create_at", description="생성일시", example=datetime.now())
     updated_at: datetime = Field(title="update_at", description="수정일시", example=datetime.now())
+
+class DomainReqAdminPostBook(BaseModel):
+    book_title: str = Field(title="book_title", description="책 제목", example="FastAPI Tutorial")
+    code: str = Field(title="code", description="책 코드", examples="A3")
+    category_name: str = Field(title="category_name", description="카테고리명", examples="웹")
+    subtitle: str | None = Field(title="subtitle", description="책 부제")
+    autor : str = Field(title="author", description="저자")
+    publisher: str = Field(title="publisher", description="출판사")
+    publication_year: int = Field(title="publication_year", description="출판연도", gt=0)
+    image_url: str | None = Field(title="image_url", description="책 표지")
+    version: str | None = Field(title="version", description="판본")
+    major: bool = Field(title="major", description="전공 책 여부")
+    language: str = Field(title="language", description="언어")
+    donor_name: str | None = Field(title="donor_name", description="기부자명")
