de11d5253486cdf3c151d9b2c6f009e6c1f6a8f8
ywnjeong <wjddusdn1237@gmail.com>
2024-11-13 21:48:36 +0900

    feat: get notices

--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ from routes.books_route import router as books_router
 from routes.loan_route import router as loan_router
 from routes.user_route import router as user_router
 from routes.admin.notice_route import router as admin_notice_router
+from routes.notice_route import router as notice_router
 
 settings = Settings()
 
@@ -50,6 +51,7 @@ app.include_router(loan_router)
 app.include_router(review_router)
 app.include_router(bookrequest_router)
 app.include_router(admin_notice_router)
+app.include_router(notice_router)
 
 
 @app.get("/")
