02af46c802b637763d07bca1b3c76150d9673fb0
임유나 <youna0104@naver.com>
2024-11-13 21:32:25 +0900

    add: route response 추가

--- src/routes/admin/admin_books_route.py
+++ src/routes/admin/admin_books_route.py
@@ -5,6 +5,7 @@ from dependencies import get_current_admin, get_db
 from domain.schemas.book_schemas import DomainReqAdminPostBook
 from domain.services.admin.book_service import service_admin_create_book
 from routes.admin.request.book_request import RouteReqAdminPostBook
+from routes.admin.response.book_response import RouteResAdminPostBook
 
 router = APIRouter(
     prefix="/admin/books",
@@ -36,5 +37,23 @@ async def create_admin_book(
       language=request.language,
       donor_name = request.donor_name if request.subtitle is not None else None
     )
-    await service_admin_create_book(domain_req, db)
-    return
+    domain_res = await service_admin_create_book(domain_req, db)
+    result = RouteResAdminPostBook(
+        book_id=domain_res.id,
+        book_title=domain_res.book_title,
+        code=domain_res.code,
+        category_name=domain_res.category_name,
+        subtitle=domain_res.subtitle,
+        author=domain_res.author,
+        publisher=domain_res.publisher,
+        publication_year=domain_res.publication_year,
+        image_url=domain_res.image_url,
+        version=domain_res.version,
+        major=domain_res.major,
+        language=domain_res.language,
+        book_status=domain_res.book_status,
+        donor_name=domain_res.donor_name,
+        created_at=domain_res.created_at,
+        updated_at=domain_res.updated_at
+    )
+    return result
