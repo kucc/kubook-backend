044097f4bfd15603c6e5f5c2812ab5199f971e1c
ywnjeong <wjddusdn1237@gmail.com>
2024-11-27 21:42:02 +0900

    fix: offset보다 적으면 400 error

--- src/domain/services/admin/notice_service.py
+++ src/domain/services/admin/notice_service.py
@@ -32,6 +32,8 @@ async def service_admin_read_notices(page: int, limit: int, db: Session):
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Notices not found")
         elif not total:
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Fetch incorrect total value")
+        elif total < offset:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Requested page is out of range")
         response = [
             DomainResAdminGetNotice(
                 notice_id=notice.id,
