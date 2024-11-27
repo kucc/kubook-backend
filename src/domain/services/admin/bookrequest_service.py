740d9325b42562d28c422842b7fad539863e8772
임유나 <youna0104@naver.com>
2024-11-28 01:49:45 +0900

    feat: domain function - delete admin bookreqeust

--- src/domain/services/admin/bookrequest_service.py
+++ src/domain/services/admin/bookrequest_service.py
@@ -3,7 +3,7 @@
 from datetime import datetime
 
 from fastapi import HTTPException, status
-from sqlalchemy import func, select
+from sqlalchemy import and_, func, select
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
@@ -53,7 +53,7 @@ async def service_admin_read_bookrequest(db: Session, page: int, limit: int):
 
 
 async def service_admin_update_bookrequest(db:Session, request: DomainReqAdminPutBookRequest):
-    stmt = select(RequestedBook).where(RequestedBook.id==request.request_id)
+    stmt = select(RequestedBook).where(and_(RequestedBook.id==request.request_id, RequestedBook.is_deleted==False))
     request_book = db.execute(stmt).scalar_one_or_none()
     if not request_book:
       raise HTTPException(
@@ -107,3 +107,34 @@ async def service_admin_update_bookrequest(db:Session, request: DomainReqAdminPu
         reject_reason=request_book.reject_reason
       )
       return result
+
+async def service_admin_delete_bookrequest(request_id: int, db: Session):
+    stmt = select(RequestedBook).where(and_(RequestedBook.id==request_id, RequestedBook.is_deleted==False))
+    request_book = db.execute(stmt).scalar_one_or_none()
+    if not request_book:
+      raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Not found"
+      )
+    try:
+      request_book.is_deleted = True
+      db.flush()
+
+    except IntegrityError as e:
+      db.rollback()
+      raise HTTPException(
+          status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+          detail=f"Integrity Error occurred during update the Review item.: {str(e)}",
+      ) from e
+
+    except Exception as e:
+      db.rollback()
+      raise HTTPException(
+          status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+          detail=f"Unexpected error occurred during update: {str(e)}",
+      ) from e
+
+    else:
+      db.commit()
+      db.refresh(request_book)
+    return
