b3cd3fabf7e366a6f7a25111a26c6888e9e01ddf
sounmu <hyginus011111@gmail.com>
2024-11-27 23:49:31 +0900

    feat: Add distinction for admin user who were deleted

--- src/domain/services/admin/user_service.py
+++ src/domain/services/admin/user_service.py
@@ -1,5 +1,6 @@
 from fastapi import HTTPException, status
 from sqlalchemy import select, text
+from sqlalchemy.exc import NoResultFound
 from sqlalchemy.orm import Session, selectinload
 
 from domain.schemas.user_schemas import DomainAdminGetUserItem, DomainReqAdminDelUser
@@ -109,5 +110,50 @@ async def service_admin_read_users(db: Session) -> list[DomainAdminGetUserItem]:
 
 
 async def service_admin_delete_user(request: DomainReqAdminDelUser, db: Session):
-    delete_item(User, request.user_id, db)
+    # 유저를 검색해서 관리자가 아니라면 바로 삭제. 관리자라면 관리자 테이블에서 이미 삭제되었는지 확인 후 삭제
+    stmt = (
+        select(User)
+        .options(
+            selectinload(User.admin)
+        )
+        .where(
+            User.id == request.user_id,
+            User.is_deleted == False
+        )
+    )
+
+    try:
+        user = db.execute(stmt).scalar_one()
+
+        if user.admin:
+            if not user.admin[0].is_deleted:
+                raise HTTPException(
+                    status_code=status.HTTP_403_FORBIDDEN,
+                    detail="Cannot delete an active admin user"
+                )
+    except NoResultFound as e:
+        raise HTTPException(
+             status_code=status.HTTP_404_NOT_FOUND,
+             detail="User not found"
+        ) from e
+    except AttributeError as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Unexpected attribute error occurred: {str(e)}",
+        ) from e
+    except HTTPException as e:
+            raise e
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Unexpected error occurred during user retrieval: {str(e)}",
+        ) from e
+    try:
+        delete_item(User, request.user_id, db)
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Unexpected error occurred during user deletion: {str(e)}",
+        ) from e
+
     return
