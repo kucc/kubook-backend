f5ae26964b6a6fa6e6d14f0a30c20de130596233
sounmu <hyginus011111@gmail.com>
2024-11-28 10:57:44 +0900

    fix: Update dependency to get_current_admin

--- src/routes/admin/loan_route.py
+++ src/routes/admin/loan_route.py
@@ -1,14 +1,14 @@
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
-from dependencies import get_current_active_user, get_db
+from dependencies import get_current_admin, get_db
 from domain.schemas.loan_schemas import DomianResGetLoanItem
 from domain.services.admin.loan_service import service_admin_return_loan
 
 router = APIRouter(
     prefix="/admin/loans",
     tags=["admin/loans"],
-    dependencies=[Depends(get_current_active_user)]
+    dependencies=[Depends(get_current_admin)]
 )
 
 
@@ -21,7 +21,7 @@ router = APIRouter(
 async def return_loan(
     loan_id: int,
     db: Session = Depends(get_db),
-    current_admin=Depends(get_current_active_user),
+    current_admin=Depends(get_current_admin),
 ):
     response = await service_admin_return_loan(loan_id, db)
 
