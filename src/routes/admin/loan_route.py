5ab2a8cb6105669fa3764b04162508826d60bca7
sounmu <hyginus011111@gmail.com>
2024-11-28 10:56:19 +0900

    refactor: Update route paths and tags

--- src/routes/admin/loan_route.py
+++ src/routes/admin/loan_route.py
@@ -3,19 +3,17 @@ from sqlalchemy.orm import Session
 
 from dependencies import get_current_active_user, get_db
 from domain.schemas.loan_schemas import DomianResGetLoanItem
-from domain.services.admin.loan_service import service_admin_return_loan, service_admin_unreturn_loan
+from domain.services.admin.loan_service import service_admin_return_loan
 
 router = APIRouter(
-    prefix="/admin",
-    tags=["admin"],
+    prefix="/admin/loans",
+    tags=["admin/loans"],
     dependencies=[Depends(get_current_active_user)]
 )
-"""
-    get_current_admin이 미완성이라 get_current_active_user에 의존성 주입함.
-"""
+
 
 @router.put(
-    "/loan/{loan_id}/return",
+    "/{loan_id}/toggle",
     response_model=DomianResGetLoanItem,
     status_code=status.HTTP_200_OK,
     summary="관리자의 대출 반납 수정"
@@ -26,20 +24,5 @@ async def return_loan(
     current_admin=Depends(get_current_active_user),
 ):
     response = await service_admin_return_loan(loan_id, db)
-
-    return response
-
-@router.put(
-    "/loans/{loan_id}/unreturn",
-    response_model=DomianResGetLoanItem,
-    status_code=status.HTTP_200_OK,
-    summary="관리자의 대출 미반납 수정"
-)
-async def unreturn_loan(
-    loan_id: int,
-    db: Session = Depends(get_db),
-    current_admin=Depends(get_current_active_user),
-):
-    response = await service_admin_unreturn_loan(loan_id, db)
 
     return response
