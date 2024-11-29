2a767eee615116ab7b0303db99685cc0d6adc393
임유나 <youna0104@naver.com>
2024-11-29 18:42:02 +0900

    fix: using enum instead 3

--- src/routes/admin/request/bookrequest_request.py
+++ src/routes/admin/request/bookrequest_request.py
@@ -4,6 +4,8 @@ from typing import Self
 from fastapi import HTTPException, status
 from pydantic import BaseModel, Field, model_validator
 
+from domain.enums.status import BookRequestStatus
+
 
 class RouteReqAdminPutBookRequest(BaseModel):
     processing_status: int = Field(0, title="processing_status", description="처리 상태", example=0)
@@ -11,7 +13,7 @@ class RouteReqAdminPutBookRequest(BaseModel):
 
     @model_validator(mode='after')
     def check_reject_reason(self) -> Self:
-        if self.processing_status == 3 and not self.reject_reason:
+        if self.processing_status == BookRequestStatus.REJECTED() and not self.reject_reason:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail="Reject reason is missed"
