c7c31eaab8f835cbdfbc5acca7e4c2a775ee1c9f
ywnjeong <wjddusdn1237@gmail.com>
2024-11-29 20:48:06 +0900

    fix: delete user dependency for get notice

--- src/routes/notice_route.py
+++ src/routes/notice_route.py
@@ -1,14 +1,13 @@
 from fastapi import APIRouter, Depends, Query, status
 from sqlalchemy.orm import Session
 
-from dependencies import get_current_active_user, get_db
+from dependencies import get_db
 from domain.services.notice_service import service_read_notice, service_read_notices
 from routes.response.notice_response import RouteResGetNotice, RouteResGetNoticeList
 
 router=APIRouter(
     prefix="/notice",
-    tags=["notice"],
-    dependencies=[Depends(get_current_active_user)]
+    tags=["notice"]
 )
 
 @router.get(
@@ -21,8 +20,7 @@ router=APIRouter(
 async def get_all_notices(
     page: int = Query(1, ge=1),
     limit: int = Query(7, le=50),
-    db: Session=Depends(get_db),
-    current_user=Depends(get_current_active_user)
+    db: Session=Depends(get_db)
 ):
     domain_res, total = await service_read_notices(page, limit, db)
     response = RouteResGetNoticeList(
@@ -43,8 +41,7 @@ async def get_all_notices(
 
 async def get_notice(
     notice_id: int,
-    db: Session=Depends(get_db),
-    current_user=Depends(get_current_active_user)
+    db: Session=Depends(get_db)
 ):
     domain_res = await service_read_notice(notice_id, db)
     response = RouteResGetNotice(
