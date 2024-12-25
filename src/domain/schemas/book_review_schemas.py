f86f33a0a8013a71983177b2cb24d7ae7bf2565f
sounmu <hyginus011111@gmail.com>
2024-12-26 00:04:38 +0900

    fix: Replace alias for datetime in book review schemas `_datetime -> datetime`

--- src/domain/schemas/book_review_schemas.py
+++ src/domain/schemas/book_review_schemas.py
@@ -1,4 +1,4 @@
-from datetime import datetime as _datetime
+from datetime import datetime
 
 from pydantic import BaseModel, Field
 
@@ -8,8 +8,8 @@ class DomainResGetReviewByInfoId(BaseModel):
     user_id: int = Field(title="user_id", description="리뷰한 사용자 ID", example=1, gt=0)
     user_name: str = Field(title="user_name", description="리뷰한 사용자 이름", example="test")
     review_content: str = Field(title="review_content", description="리뷰 내용")
-    created_at: _datetime = Field(title="create_at", description="생성일시", example=_datetime.now())
-    updated_at: _datetime = Field(title="update_at", description="수정일시", example=_datetime.now())
+    created_at: datetime = Field(title="create_at", description="생성일시", example=datetime.now())
+    updated_at: datetime = Field(title="update_at", description="수정일시", example=datetime.now())
 
 
 class Review(BaseModel):
@@ -17,8 +17,8 @@ class Review(BaseModel):
     user_id: int = Field(title="user_id", description="리뷰한 사용자 ID", example=1, gt=0)
     book_id: int = Field(title="book_id", description="리뷰한 책 정보 ID", example=1, gt=0)
     review_content: str = Field(title="review_content", description="리뷰 내용")
-    created_at: _datetime = Field(title="create_at", description="생성일시", example=_datetime.now())
-    updated_at: _datetime = Field(title="update_at", description="수정일시", example=_datetime.now())
+    created_at: datetime = Field(title="create_at", description="생성일시", example=datetime.now())
+    updated_at: datetime = Field(title="update_at", description="수정일시", example=datetime.now())
 
 
 class DomainResGetReviewItem(BaseModel):
@@ -26,8 +26,8 @@ class DomainResGetReviewItem(BaseModel):
     user_id: int = Field(title="user_id", description="리뷰한 사용자 ID", example=1, gt=0)
     book_id: int = Field(title="book_id", description="리뷰한 책 정보 ID", example=1, gt=0)
     review_content: str = Field(title="review_content", description="리뷰 내용")
-    created_at: _datetime = Field(title="create_at", description="생성일시", example=_datetime.now())
-    updated_at: _datetime = Field(title="update_at", description="수정일시", example=_datetime.now())
+    created_at: datetime = Field(title="create_at", description="생성일시", example=datetime.now())
+    updated_at: datetime = Field(title="update_at", description="수정일시", example=datetime.now())
     book_title: str = Field(title="book_title", description="책 제목", example="FastAPI Tutorial")
 
 
@@ -43,7 +43,7 @@ class DomainResPostReview(BaseModel):
     user_name: str = Field(title="user_name", description="리뷰한 사용자 이름")
     book_id: int = Field(title="book_id", description="리뷰한 책 정보 ID", example=1, gt=0)
     review_content: str = Field(title="review_content", description="리뷰 내용")
-    created_at: _datetime = Field(title="create_at", description="생성일시", example=_datetime.now())
+    created_at: datetime = Field(title="create_at", description="생성일시", example=datetime.now())
 
 class DomainReqPutReview(BaseModel):
     review_id: int = Field(title="book_review_id", description="리뷰 id", example=1, gt=0)
