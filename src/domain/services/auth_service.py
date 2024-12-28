c55d5873b5a53da9907ae5fdc8c1d3ed7128c72f
smreosms13 <smreosms13@gmail.com>
2024-12-29 01:14:56 +0900

    fix: add auth_schema and refactator service_refresh_token

--- src/domain/services/auth_service.py
+++ src/domain/services/auth_service.py
@@ -4,8 +4,8 @@ from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 
 from config import Settings
-from domain.schemas.auth_schemas import LoginRequest, RegisterRequest
-from domain.services.token_service import create_user_tokens, refresh_user_tokens
+from domain.schemas.auth_schemas import LoginRequest, LoginResponse, RegisterRequest, RegisterResponse, UserInfo
+from domain.services.token_service import create_user_tokens, refresh_user_tokens, verify_token
 from externals.firebase import sign_in_with_email_and_password
 from repositories.models import User
 
@@ -36,12 +36,16 @@ async def register(request: RegisterRequest, db: Session):
 
     # Create JWT tokens
     token_response = create_user_tokens(user.id)
-    response = JSONResponse(content={
-        "id": user.id,
-        "user_name": user.user_name,
-        "is_active": user.is_active,
-        "email": user.email
-    }, status_code=status.HTTP_201_CREATED)
+    user_info = RegisterResponse(
+        user=UserInfo(
+            id=user.id,
+            user_name=user.user_name,
+            email=user.email,
+            is_active=user.is_active
+        )
+    ).model_dump()
+    response = JSONResponse(content=user_info, status_code=status.HTTP_201_CREATED)
+
     response.headers["Authorization"] = token_response["access_token"]
     response.set_cookie(key="refresh_token", value=token_response["refresh_token"],
                         httponly = True, secure=False, samesite="Lax")
@@ -50,7 +54,7 @@ async def register(request: RegisterRequest, db: Session):
 # firebase를 사용한 로그인
 
 
-async def login_with_firebase(request, db: Session):
+async def login_with_firebase(request:LoginRequest, db: Session):
     # Authenticate user
     # Check if user exists in Firebase
 
@@ -58,7 +62,7 @@ async def login_with_firebase(request, db: Session):
     local_id = firebase_response["localId"]
 
     # Check if user information exists in the DB
-    user = db.query(User).filter(User.auth_id == request.auth_id).first()
+    user = db.query(User).filter(User.auth_id == request.auth_id, User.is_deleted==False).first()
 
     # If user information does not exist in the DB, create a new user
     user_info_required = False
@@ -75,22 +79,20 @@ async def login_with_firebase(request, db: Session):
 
     # Create JWT tokens
     token_response = create_user_tokens(user.id)
+    login_response = LoginResponse(
+        user_info_required,
+        user=UserInfo(
+            id=user.id,
+            user_name=user.user_name,
+            email=user.email,
+            is_active=user.is_active
+        )
+    ).model_dump()
+    response = JSONResponse(content=login_response, status_code=status.HTTP_200_OK)
+    response.headers["Authorization"] = token_response["access_token"]
+    response.set_cookie(key="refresh_token", value=token_response["refresh_token"])
 
-    if user_info_required:
-        return {
-            "token": token_response,
-            "user_info_required": True
-        }
-    else:
-        return {
-            "token": token_response,
-            "user": {
-                "id": user.id,
-                "user_name": user.user_name,
-                "is_active": user.is_active,
-                "email": user.email
-            }
-        }
+    return response
 
 
 async def login_with_username(
@@ -98,33 +100,47 @@ async def login_with_username(
         db: Session):
     # Authenticate user
     # Check if user information exists in the DB
-    user = db.query(User).filter(User.email == request.email).first()
-
+    user = db.query(User).filter(User.email == request.email, User.is_deleted==False).first()
     # If user information does not exist in the DB, return error
     if user is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
 
     if not pwd_context.verify(request.password, user.password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unvalid Password")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Password")
 
     # Create JWT tokens
     token_response = create_user_tokens(user.id)
-    response = JSONResponse(content={
-        "id": user.id,
-        "user_name": user.user_name,
-        "is_active": user.is_active,
-        "email": user.email
-    }, status_code=status.HTTP_200_OK)
+    login_response = LoginResponse(
+        user_info_required=True,
+        user=UserInfo(
+            id=user.id,
+            user_name=user.user_name,
+            email=user.email,
+            is_active=user.is_active
+        )
+    ).model_dump()
+    response = JSONResponse(content=login_response, status_code=status.HTTP_200_OK, headers={
+        "Authorization" : token_response["access_token"]
+    })
     response.headers["Authorization"] = token_response["access_token"]
     response.set_cookie(key="refresh_token", value=token_response["refresh_token"])
     return response
 
 async def service_refresh_token(access_token: str, refresh_token: str):
-    try :
-        token_response = refresh_user_tokens(access_token, refresh_token)
+    try:
+        verified_access = verify_token(access_token)
+        if verified_access > -1 :
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Available Access Token",
+                headers={"WWW-Authenticate": "Bearer"}
+            )
+        elif verified_access == -1 :
+            token_response = refresh_user_tokens(refresh_token)
     except HTTPException as e:
         raise e
-    response = JSONResponse(content=None, status_code=status.HTTP_202_ACCEPTED)
-    response.headers["Authorization"]= token_response["access_token"]
+    response = JSONResponse(content=None, status_code=status.HTTP_201_CREATED, headers={
+        "Authorization" : token_response["access_token"]
+    })
     response.set_cookie(key="refresh_token", value=token_response["refresh_token"])
     return response
